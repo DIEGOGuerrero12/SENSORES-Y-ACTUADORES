fcd9f7df1e65d70a93f4a2df061538f557bc0844
DIEGOGuerrero12 <160614103+DIEGOGuerrero12@users.noreply.github.com>
2025-03-10 18:50:08 +0000

    base de datos

--- sensores/sensores.js
+++ sensores/sensores.js
@@ -1,3 +1,21 @@
 function getDatos(){
-    alert("si funciona");
+    console.log("Si funciona");
+    const url = "https://diego-50c40-default-rtdb.firebaseio.com/sensores.json";
+    const requests =  new XMLHttpRequest();
+
+    requests.open("GET", url);
+
+    requests.onload = function(){
+        const data = JSON.parse(this.responseText)
+
+        const temperatura = data.temperatura;
+        const humedad = data.humedad;
+
+        console.log(data);
+
+        document.getElementById("tempeatura").innerText = ` ${temperatura} °C` ;
+        document.getElementById("humedad").innerText =`  ${humedad} %` ;
+    }
+
+    requests.send();
 };
